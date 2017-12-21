1932575352337fe18c76f2e6132ff6428486cacf
Taras <taras.prezlyata.sign.in@gmail.com>
2017-12-22 00:49:42 +0200

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
+import Header from '../components/header/header.js';
+
 
 export class App extends Component  {
   render() {
     return (
         <div className="app">
-          <h1>Hello World!</h1>
-          <p>Foo to the bar</p>
-          <h2>React test</h2>
+          <Header/>
         </div>
     );
   }
 }
-
---
+++ src/components/header/header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './header.scss';
+
+
+class Header extends Component{
+    render() {
+        return(
+            <div>
+                <h1 className="text">ТарssssасБ</h1>
+                <h2 className="text2">fdfsdssssssfdf</h2>
+            </div>
+        );
+    } 
+};
+
+export default Header;
